c0659fa168615002eb4f57a7bec5cdfb65cac2e3
YasiruVK <145313332+yasiru-vk@users.noreply.github.com>
2024-03-29 09:55:34 +0530

    Creted navbar component

---
+++ portfolio-project/src/components/navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Navbar.scss"
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className='wrapper'>
+            <span>Yasiru.</span>
+            <div className='links'>
+                <a href="#">Me</a>
+                <a href="#">Education</a>
+                <a href="#">Skills</a>
+                <a href="#">Projects</a>
+                <a href="#">Contact Me</a>
+            </div>
+
+        </div>
+
+
+    </div>
+    
+    
+  )
+}
+
+export default Navbar
